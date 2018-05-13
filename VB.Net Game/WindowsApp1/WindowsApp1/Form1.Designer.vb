b78657480a55218d77efcd69fb7f662860c258b8
Thalomir <39244472+Thalomir@users.noreply.github.com>
2018-05-13 15:35:50 -0400

    Add files via upload

---
+++ VB.Net Game/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Ply = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.tmrRight = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrLeft = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrUp = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrDown = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        CType(Me.Ply, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Ply
+        '
+        Me.Ply.BackColor = System.Drawing.Color.Black
+        Me.Ply.Location = New System.Drawing.Point(367, 170)
+        Me.Ply.Name = "Ply"
+        Me.Ply.Size = New System.Drawing.Size(32, 32)
+        Me.Ply.TabIndex = 0
+        Me.Ply.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Red
+        Me.PictureBox2.Location = New System.Drawing.Point(900, 200)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(83, 52)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Red
+        Me.PictureBox3.Location = New System.Drawing.Point(900, 100)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(83, 52)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'tmrRight
+        '
+        Me.tmrRight.Interval = 10
+        '
+        'tmrLeft
+        '
+        Me.tmrLeft.Interval = 10
+        '
+        'tmrUp
+        '
+        Me.tmrUp.Interval = 10
+        '
+        'tmrDown
+        '
+        Me.tmrDown.Interval = 10
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Red
+        Me.PictureBox4.Location = New System.Drawing.Point(323, 500)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(76, 45)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Red
+        Me.PictureBox5.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(69, 40)
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 411)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Ply)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Ply, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Ply As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents tmrRight As Timer
+    Friend WithEvents tmrLeft As Timer
+    Friend WithEvents tmrUp As Timer
+    Friend WithEvents tmrDown As Timer
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+End Class
---
+++ VB.Net Game/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+    Dim Avatar As player
+    Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AutoScroll = False
+
+
+    End Sub
+    Structure player
+        Dim Score As Integer
+        Dim inv() As String
+        Dim i As Integer
+        Dim Left As Double
+        Dim Right As Double
+        Dim Top As Double
+        Dim Bottom As Double
+
+        Function RCol() As Boolean
+            Top = Form1.Ply.Top
+            Bottom = Form1.Ply.Top + Form1.Ply.Height
+            Left = Form1.Ply.Left
+            Right = Form1.Ply.Left + Form1.Ply.Width
+            Dim objects() As PictureBox = {Form1.PictureBox2, Form1.PictureBox3, Form1.PictureBox4, Form1.PictureBox5}
+
+            ' For i = 1 To (objects.Length - 1)
+            '   If Right >= objects(i).Left Then
+            '  If Top < objects(i).Top And Top < objects(i).Top + objects(i).Height Then
+            '     Return True
+            ' End If
+            ' If Bottom > objects(i).Top + objects(i).Height And Bottom < objects(i).Top Then
+            '    Return True
+            ' End If
+            ' Else
+            ' Return False
+
+            ' End If
+            'Next
+
+
+        End Function
+
+    End Structure
+
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.D
+                tmrLeft.Enabled = False
+                tmrRight.Enabled = True
+            Case Keys.A
+                tmrRight.Enabled = False
+                tmrLeft.Enabled = True
+            Case Keys.W
+                tmrDown.Enabled = False
+                tmrUp.Enabled = True
+            Case Keys.S
+                tmrUp.Enabled = False
+                tmrDown.Enabled = True
+        End Select
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        Select Case e.KeyCode
+            Case Keys.D
+                tmrRight.Enabled = False
+            Case Keys.A
+                tmrLeft.Enabled = False
+            Case Keys.W
+                tmrUp.Enabled = False
+            Case Keys.S
+                tmrDown.Enabled = False
+        End Select
+    End Sub
+
+    Private Sub tmrRight_Tick(sender As Object, e As EventArgs) Handles tmrRight.Tick
+        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+
+        For x = 0 To objects.Length - 1
+            objects(x).Left -= 3
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                objects(x).Left += 3
+            Else
+
+            End If
+        Next
+    End Sub
+
+    Private Sub tmrLeft_Tick(sender As Object, e As EventArgs) Handles tmrLeft.Tick
+        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+
+        For x = 0 To objects.Length - 1
+            objects(x).Left += 3
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                objects(x).Left -= 3
+            Else
+
+            End If
+        Next
+    End Sub
+
+    Private Sub tmrUp_Tick(sender As Object, e As EventArgs) Handles tmrUp.Tick
+        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+
+        For x = 0 To objects.Length - 1
+            objects(x).Top += 3
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                objects(x).Top -= 3
+            Else
+
+            End If
+        Next
+    End Sub
+
+    Private Sub tmrDown_Tick(sender As Object, e As EventArgs) Handles tmrDown.Tick
+        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+
+        For x = 0 To objects.Length - 1
+            objects(x).Top -= 3
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                objects(x).Top += 3
+            Else
+
+            End If
+        Next
+    End Sub
+
+End Class
