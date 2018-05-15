79b77f257d2f04f21bd3239874fa49107429fc28
Thalomir <39244472+Thalomir@users.noreply.github.com>
2018-05-15 11:40:41 -0400

    Add files via upload

--- VB.Net Game/WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ VB.Net Game/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -24,47 +24,32 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Ply = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.tmrRight = New System.Windows.Forms.Timer(Me.components)
         Me.tmrLeft = New System.Windows.Forms.Timer(Me.components)
         Me.tmrUp = New System.Windows.Forms.Timer(Me.components)
         Me.tmrDown = New System.Windows.Forms.Timer(Me.components)
+        Me.timLazers = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.PictureBox4 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.lblLaser = New System.Windows.Forms.PictureBox()
         CType(Me.Ply, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.lblLaser, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Ply
         '
-        Me.Ply.BackColor = System.Drawing.Color.Black
-        Me.Ply.Location = New System.Drawing.Point(367, 170)
+        Me.Ply.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Ply.Location = New System.Drawing.Point(339, 199)
         Me.Ply.Name = "Ply"
-        Me.Ply.Size = New System.Drawing.Size(32, 32)
+        Me.Ply.Size = New System.Drawing.Size(34, 32)
         Me.Ply.TabIndex = 0
         Me.Ply.TabStop = False
-        '
-        'PictureBox2
-        '
-        Me.PictureBox2.BackColor = System.Drawing.Color.Red
-        Me.PictureBox2.Location = New System.Drawing.Point(900, 200)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(83, 52)
-        Me.PictureBox2.TabIndex = 1
-        Me.PictureBox2.TabStop = False
-        '
-        'PictureBox3
-        '
-        Me.PictureBox3.BackColor = System.Drawing.Color.Red
-        Me.PictureBox3.Location = New System.Drawing.Point(900, 100)
-        Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(83, 52)
-        Me.PictureBox3.TabIndex = 2
-        Me.PictureBox3.TabStop = False
         '
         'tmrRight
         '
@@ -82,52 +67,87 @@ Partial Class Form1
         '
         Me.tmrDown.Interval = 10
         '
+        'timLazers
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox1.Location = New System.Drawing.Point(-317, -2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(347, 1124)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox2.Location = New System.Drawing.Point(-15, 1029)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(1925, 615)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox3.Location = New System.Drawing.Point(1860, -96)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(878, 1152)
+        Me.PictureBox3.TabIndex = 3
+        Me.PictureBox3.TabStop = False
+        '
         'PictureBox4
         '
-        Me.PictureBox4.BackColor = System.Drawing.Color.Red
-        Me.PictureBox4.Location = New System.Drawing.Point(323, 500)
+        Me.PictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox4.Location = New System.Drawing.Point(-71, -435)
         Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(76, 45)
-        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.Size = New System.Drawing.Size(1964, 471)
+        Me.PictureBox4.TabIndex = 4
         Me.PictureBox4.TabStop = False
         '
-        'PictureBox5
+        'lblLaser
         '
-        Me.PictureBox5.BackColor = System.Drawing.Color.Red
-        Me.PictureBox5.Location = New System.Drawing.Point(0, 0)
-        Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(69, 40)
-        Me.PictureBox5.TabIndex = 4
-        Me.PictureBox5.TabStop = False
+        Me.lblLaser.BackColor = System.Drawing.Color.Red
+        Me.lblLaser.Location = New System.Drawing.Point(352, 199)
+        Me.lblLaser.Name = "lblLaser"
+        Me.lblLaser.Size = New System.Drawing.Size(10, 10)
+        Me.lblLaser.TabIndex = 5
+        Me.lblLaser.TabStop = False
+        Me.lblLaser.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(784, 411)
-        Me.Controls.Add(Me.PictureBox5)
+        Me.ClientSize = New System.Drawing.Size(705, 421)
+        Me.Controls.Add(Me.lblLaser)
         Me.Controls.Add(Me.PictureBox4)
         Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Ply)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.Ply, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.lblLaser, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Ply As PictureBox
-    Friend WithEvents PictureBox2 As PictureBox
-    Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents tmrRight As Timer
     Friend WithEvents tmrLeft As Timer
     Friend WithEvents tmrUp As Timer
     Friend WithEvents tmrDown As Timer
+    Friend WithEvents timLazers As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents PictureBox4 As PictureBox
-    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents lblLaser As PictureBox
 End Class
--- VB.Net Game/WindowsApp1/WindowsApp1/Form1.vb
+++ VB.Net Game/WindowsApp1/WindowsApp1/Form1.vb
@@ -1,11 +1,47 @@
 ﻿Public Class Form1
     Dim Avatar As player
-    Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+    Dim objects() As PictureBox = {PictureBox1, PictureBox2, PictureBox3, PictureBox4}
+    Public Collisions(55) As Sides
+    Dim RCol As Boolean
+    Dim LCol As Boolean
+    Dim UCol As Boolean
+    Dim DCol As Boolean
+    Dim c As Integer
+    Dim bullet As laser
+    Dim i As Integer
+
+
+
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.AutoScroll = False
-
-
+        objects(0) = Me.PictureBox1
+        objects(1) = Me.PictureBox2
+        objects(2) = Me.PictureBox3
+        objects(3) = Me.PictureBox4
     End Sub
+    Structure laser
+
+        Dim x1 As Integer
+        Dim y1 As Integer
+        Dim x2 As Integer
+        Dim y2 As Integer
+        Dim slopex As Integer
+        Dim slopey As Integer
+        Dim slopexint As Integer
+        Dim slopeyint As Integer
+
+    End Structure
+
+    Structure Sides
+        Dim Right As Boolean
+        Dim Left As Boolean
+        Dim Top As Boolean
+        Dim Bottom As Boolean
+    End Structure
+
+
+
     Structure player
         Dim Score As Integer
         Dim inv() As String
@@ -14,30 +50,6 @@
         Dim Right As Double
         Dim Top As Double
         Dim Bottom As Double
-
-        Function RCol() As Boolean
-            Top = Form1.Ply.Top
-            Bottom = Form1.Ply.Top + Form1.Ply.Height
-            Left = Form1.Ply.Left
-            Right = Form1.Ply.Left + Form1.Ply.Width
-            Dim objects() As PictureBox = {Form1.PictureBox2, Form1.PictureBox3, Form1.PictureBox4, Form1.PictureBox5}
-
-            ' For i = 1 To (objects.Length - 1)
-            '   If Right >= objects(i).Left Then
-            '  If Top < objects(i).Top And Top < objects(i).Top + objects(i).Height Then
-            '     Return True
-            ' End If
-            ' If Bottom > objects(i).Top + objects(i).Height And Bottom < objects(i).Top Then
-            '    Return True
-            ' End If
-            ' Else
-            ' Return False
-
-            ' End If
-            'Next
-
-
-        End Function
 
     End Structure
 
@@ -74,55 +86,126 @@
     End Sub
 
     Private Sub tmrRight_Tick(sender As Object, e As EventArgs) Handles tmrRight.Tick
-        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+        Dim objects() As PictureBox = {PictureBox1, PictureBox2, PictureBox3, PictureBox4}
 
         For x = 0 To objects.Length - 1
             objects(x).Left -= 3
-            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
-                objects(x).Left += 3
-            Else
+        Next
 
+        For x = 0 To objects.Length - 1
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                c += 1
             End If
         Next
+
+        If c <> 0 Then
+            For x = 0 To objects.Length - 1
+                objects(x).Left += 6
+            Next
+        End If
+
+        c = 0
+
     End Sub
 
     Private Sub tmrLeft_Tick(sender As Object, e As EventArgs) Handles tmrLeft.Tick
-        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+        Dim objects() As PictureBox = {PictureBox1, PictureBox2, PictureBox3, PictureBox4}
+
 
         For x = 0 To objects.Length - 1
             objects(x).Left += 3
-            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
-                objects(x).Left -= 3
-            Else
+        Next
 
+        For x = 0 To objects.Length - 1
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                c += 1
             End If
         Next
+
+        If c <> 0 Then
+            For x = 0 To objects.Length - 1
+                objects(x).Left -= 6
+            Next
+        End If
+
+        c = 0
+
     End Sub
 
     Private Sub tmrUp_Tick(sender As Object, e As EventArgs) Handles tmrUp.Tick
-        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+        Dim objects() As PictureBox = {PictureBox1, PictureBox2, PictureBox3, PictureBox4}
 
         For x = 0 To objects.Length - 1
             objects(x).Top += 3
-            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
-                objects(x).Top -= 3
-            Else
+        Next
 
+        For x = 0 To objects.Length - 1
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                c += 1
             End If
         Next
+
+        If c <> 0 Then
+            For X = 0 To objects.Length - 1
+                objects(X).Top -= 6
+            Next
+        End If
+
+        c = 0
+
     End Sub
 
     Private Sub tmrDown_Tick(sender As Object, e As EventArgs) Handles tmrDown.Tick
-        Dim objects() As PictureBox = {PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+        Dim objects() As PictureBox = {PictureBox1, PictureBox2, PictureBox3, PictureBox4}
 
         For x = 0 To objects.Length - 1
             objects(x).Top -= 3
-            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
-                objects(x).Top += 3
-            Else
+        Next
 
+        For x = 0 To objects.Length - 1
+            If Ply.Bounds.IntersectsWith(objects(x).Bounds) Then
+                c += 1
             End If
         Next
+
+        If c <> 0 Then
+            For X = 0 To objects.Length - 1
+                objects(X).Top += 6
+            Next
+        End If
+
+        c = 0
+
     End Sub
 
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+
+        lblLaser.Left = Ply.Left
+        lblLaser.Top = Ply.Top
+        bullet.x1 = Ply.Location.X
+        bullet.y1 = Ply.Location.Y
+        bullet.x2 = Cursor.Position.X
+        bullet.y2 = Cursor.Position.Y
+        bullet.slopex = bullet.x2 - bullet.x1
+        bullet.slopey = bullet.y2 - bullet.y2
+        i = 0
+        timLazers.Enabled = True
+        lblLaser.Visible = True
+
+    End Sub
+
+    Private Sub timLazers_Tick(sender As Object, e As EventArgs) Handles timLazers.Tick
+
+        If lblLaser.Location.X <> bullet.x2 Then
+            lblLaser.Left += bullet.slopex
+            lblLaser.Top += bullet.slopey
+        ElseIf lblLaser.Location.X = bullet.x2 Then
+            lblLaser.Visible = False
+            lblLaser.Top = Ply.Top
+            lblLaser.Left = Ply.Left
+            i = 0
+            timLazers.Enabled = False
+        End If
+
+    End Sub
 End Class
